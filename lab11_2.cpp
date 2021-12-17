6b132d6a9d17b8123bb817e75eaffffde9014d67
Salisatoru <95071118+Salisatoru@users.noreply.github.com>
2021-12-17 16:24:50 +0700

    lab11_2.cpp.completed

---
+++ lab11_2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main (){
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt");
+	dest.open("cheerbook_copy.txt");
+	dest << "-------------------- BOOM ---------------------\n";
+	dest << "AR CHI\n";
+	dest << "AR BOW\n";
+	dest << "CHOW CHOW\n";
+	dest << "BA KA\n";
+	dest << "BA GEAR\n";
+	dest << "CHEER CHEER\n";
+	dest << "WE ARE ALL\n";
+	dest << "ENTANEER\n";
+	dest << "CMU\n";
+	
+
+    source.close();
+    dest.close();
+    cout << "-------------------- HA!! ---------------------";
+	return 0;
+}
